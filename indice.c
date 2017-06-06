bc78b9208d76954269668e862a3ddfa4991573d2
Matheus Barbosa <matheusbarbosa@hokusai.llp.dcc.ufmg.br>
2017-06-06 17:16:31 -0300

    Added unfinished functions QuickSort, Min and sortIndex

--- indice.c
+++ indice.c
@@ -10,16 +10,46 @@ void updateIndex(FILE **index, FILE **data, int memLimit, int file){
 		if(palavra[count] == ' ' || palavra[count] == '\n')	{
 			palavra[count] = '\0';
 			fprintf(*index, "%s %d %d %d\n", palavra, file,1, globalCount-count);
-			count = 0;
-		}else{
-			globalCount++;
-			count++;
+			count = -1;
 		}
 
-		
+		globalCount++;
+		count++;
 	}
 }
 
-void sortIndex(FILE **index, FILE **data, int memLimit, int file){
-	
+void sortIndex(FILE **index, int memLimit, int file){
+	int i, maxBuf = memLimit/sizeof(Index);
+	Index *indexes = (Index*) malloc(maxBuf* sizeof(Index));
+	for(i=0; i<maxBuf; i++){
+		fscanf(*index,"%s", indexes[i].word);
+		fscanf(*index,"%d", &(indexes[i].document));
+		fscanf(*index,"%d", &(indexes[i].frequency));
+		fscanf(*index,"%d", &(indexes[i].position));
+	} 
+
+	quickSort(indexes, 0, maxBuf);
+}
+
+void quickSort(Index *indexes, int front, int back){
+	char *pivo = min(indexes[0].word,indexes[back].word);
+	int i = front, j=back;
+
+	while(0){
+
+	}
+
+}
+
+char* min(char *wordA, char *wordB){
+	int i=0;
+	while(wordA[i]!='\0' && wordB[i]!='\0'){
+		if(((int) wordA[i]) < ((int) wordB[i])){
+			return wordA;
+		}else if(((int) wordB[i]) < ((int) wordA[i])){
+			return wordB;
+		}else{
+			i++;
+		}
+	}
 }
--- indice.h
+++ indice.h
@@ -1,3 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef struct index{
+	char word[20];
+	int document;
+	int frequency;
+	int position;
+} Index;
+
 
 void updateIndex(FILE **index, FILE **data, int memLimit, int file);
+void sortIndex(FILE **index, int memLimit, int file);
+void quickSort(Index *indexes, int front, int back);
+char* min(char *wordA, char *wordB);
